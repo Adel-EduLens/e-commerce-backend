1d6b4c2a826f537d89c92a871c9e7ca2c64231c0
UsamaEdulens1 <osama@edu-lens.com>
2026-07-01 12:14:24 +0300

    testing

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,28 +1,28 @@
-import type { Document } from 'mongoose';
-import type { Request } from 'express';
+import type { Document } from 'mongoose'
+import type { Request } from 'express'
 
 export interface IUser extends Document {
-  name: string;
-  email: string;
-  password: string;
-  role: 'user' | 'trader';
-  phone?: string | undefined;
-  createdAt: Date;
-  updatedAt: Date;
-  comparePassword(password: string): Promise<boolean>;
+  name: string
+  email: string
+  password: string
+  role: 'user' | 'trader'
+  phone?: string | undefined
+  createdAt: Date
+  updatedAt: Date
+  comparePassword(password: string): Promise<boolean>
 }
 
 export interface IUserResponse {
-  _id: string;
-  name: string;
-  email: string;
-  role: 'user' | 'trader';
-  phone?: string | undefined;
-  createdAt: Date;
-  updatedAt: Date;
+  _id: string
+  name: string
+  email: string
+  role: 'user' | 'trader'
+  phone?: string | undefined
+  createdAt: Date
+  updatedAt: Date
+  //hi
 }
 
 export interface AuthenticatedRequest extends Request {
-  user?: IUserResponse;
+  user?: IUserResponse
 }
-
