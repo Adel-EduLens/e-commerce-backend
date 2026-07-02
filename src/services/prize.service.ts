2d719a8151498544813a170ed9b64c5f14258403
mohamedtarek2224 <mohamed.tarek@edu-lens.com>
2026-07-02 16:30:40 +0300

    test

--- src/services/prize.service.ts
+++ src/services/prize.service.ts
@@ -1,10 +1,33 @@
-
-import { userRepository } from "../repositories/user.repository.js";
-import prisma from "../utils/prismaClient.js";
+import { Prisma } from "@prisma/client";
 import { prizeRepository } from "../repositories/prize.repository.js";
+import AppError from "../utils/AppError.util.js";
 export const prizeService = {
-    async getPrizes() {
-        const prizes = await prizeRepository.getAll();
-        return prizes;
+  async getPrizes() {
+    const prizes = await prizeRepository.getAll();
+    return prizes;
+  },
+  async addPrize(prize: Prisma.PrizeCreateInput) {
+    const prizeData = await prizeRepository.create({
+        name: prize.name,
+        weight: prize.weight,
+    });
+    return prizeData;
+  },
+  async deletePrize(id: string) {
+    const prizeData = await prizeRepository.deleteById(id);
+    return prizeData;
+  },
+  async spinPrize() {
+    const prizes = await prizeRepository.getAll();
+    if (prizes.length === 0) throw new AppError("No prizes available", 400);
+    const totalWeight = prizes.reduce((sum, prize) => sum + prize.weight, 0);
+    let random = Math.random() * totalWeight;
+    for (const prize of prizes) {
+      if (random < prize.weight) {
+        return prize;
+      }
+      random -= prize.weight;
     }
-}
+    return null;
+  },
+};
